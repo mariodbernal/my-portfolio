5acb2c745e1fe48aeb97c4f786a57a2468fb778b
Mario Bernal <58664862+mariodbernal@users.noreply.github.com>
2026-07-06 13:03:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         aether: {
             title: "Aether Campaign",
             images: [
-                "images/print/Print_AD_Design_TCC_1.png",
+                "print/Print_AD_Design_TCC_1.png",
                 "https://images.unsplash.com/photo-1618005198143-e5283b519a7f?q=80&w=1200",
                 "https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?q=80&w=1200"
             ]
